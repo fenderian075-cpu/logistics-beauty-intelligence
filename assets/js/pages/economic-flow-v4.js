547100145151dbade7686588f2829ef136370ae5
fenderian075-cpu <fenderian075@gmail.com>
2026-08-23 13:50:31 +0900

    Mount fuel and ocean freight trend panels

--- assets/js/pages/economic-flow-v4.js
+++ assets/js/pages/economic-flow-v4.js
@@ -1,7 +1,9 @@
 import { init as initBase } from "./economic-flow.js";
 import { mountIndustryDeflatorPanel } from "../render/industry-deflator-panel.js";
+import { mountCostTrendPanel } from "../render/cost-trend-panel.js";
 
 export async function init() {
   await initBase();
+  await mountCostTrendPanel();
   await mountIndustryDeflatorPanel();
 }
