8725511e8d1fdae9d67763b898b34e51a370e191
fenderian075-cpu <fenderian075@gmail.com>
2026-08-23 14:02:00 +0900

    Apply Japanese Economic Flow display contract

--- assets/js/pages/economic-flow-v4.js
+++ assets/js/pages/economic-flow-v4.js
@@ -1,9 +1,11 @@
 import { init as initBase } from "./economic-flow.js";
 import { mountIndustryDeflatorPanel } from "../render/industry-deflator-panel.js";
 import { mountCostTrendPanel } from "../render/cost-trend-panel.js";
+import { localizeEconomyDisplay } from "../render/economy-display-ja.js";
 
 export async function init() {
   await initBase();
   await mountCostTrendPanel();
   await mountIndustryDeflatorPanel();
+  localizeEconomyDisplay();
 }
